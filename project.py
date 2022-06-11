087147d85b7cee46f256caeba05bb9fb0ebf105f
root <root@Carrots.localdomain>
2022-06-11 12:58:26 +0800

    Prompt the user for name and add validate_name() function

--- project.py
+++ project.py
@@ -1,10 +1,31 @@
 import os
 
+DIV = "~" * 20
+
+
 def main():
-    ...
+    clear()
+    print("🎥 Welcome to [IMDb]Man!~~ 🎥", DIV, sep="\n")
+    while True:
+        name = input("Tell me your name: ")
+        if validate_name(name):
+            clear()
+            print(f"Hello, {name}! Choose the game's difficulty.")
+            break
+        else:
+            print(DIV, "Your name should be 4 to 10 characters long and only consist of alphanumeric characters. I'll ask you again.", DIV, sep="\n")
+
+
+def validate_name(name):
+    if 10 >= len(name) >= 4 and name.isalnum():
+        return True
+    else:
+        return False
+
 
 def clear():
     os.system("cls||clear")
+
 
 if __name__ == "__main__":
     main()
