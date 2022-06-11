2d5d1e4fc35246a2b469872d5d027621caca3fda
root <root@Carrots.localdomain>
2022-06-11 18:06:48 +0800

    Add game_area() and get_alpha() functions

--- project.py
+++ project.py
@@ -2,9 +2,12 @@ from rich.console import Console
 
 import scraper
 import os
-
+import random
+import re
 
 console = Console()
+
+
 
 # A str that acts as a divider
 DIV = "~" * 20
@@ -15,6 +18,12 @@ DIF = f"""
 [2] Hmm, so you like movies a lot, huh? Interesting.
 [3] You know these movies?!
 """
+
+# Initial number of lives per start of a round
+MAX_LIFE = 7
+
+# Name of the csv file that will store players' usernames and scores
+SCOREBOARD = "scoreboard.csv"
 
 
 def main():
@@ -65,11 +74,14 @@ def main():
     # Gather movie data
     clear()
     with console.status("Gathering movies for you. Stay still...", spinner="arrow3"):
-        movies = scraper.get_movies(diff)
-        for movie in movies:
-            if movie["title"].isdigit():
-                index = movies.index(movie)
-                movies.remove(movies[index])
+        movies_raw = scraper.get_movies(diff)
+        movies = random.sample(movies_raw, 200)
+
+
+    total_score = 0
+    round = 1
+    while len(movies) != 0:
+        round_stats = game_area(movies, round)
 
 
 def validate_name(name):
@@ -106,6 +118,80 @@ def validate_diff(get_diff):
             return False
     except ValueError:
         raise ValueError
+
+
+def game_area(movies, round):
+    """Executes the game
+
+    Args:
+        movies (list): a list of dictionary with the following keys: 'title', 'year', and 'genre'
+        round (int): current round of the game, starts at 1
+
+    Returns:
+        dict: returns a dict which consists of the round's title to be guessed, remaining lives, score, and index of the guessed movie
+    """
+    clear()
+    round_movie = random.choice(movies)
+    title, year, genre = round_movie['title'], round_movie['year'], round_movie['genre']
+    life = MAX_LIFE
+    alpha_text = set(get_alpha(title))
+    entered_char = []
+    goal = len(alpha_text)
+    score = 0
+    while True:
+        clear()
+        print(DIV, f"Round {round}", f"Year: {year}", f"Genre: {genre}", DIV, sep="\n")
+        print("Title: ", end="")
+        for char in title.upper():
+            if char.isalpha() and char not in entered_char:
+                print("_", end="")
+            else:
+                print(char, end="")
+        print("\nEntered letters: ", end="")
+        for char in entered_char:
+            if char in alpha_text:
+                console.print(char, style="bold green", end="")
+            else:
+                console.print(char, style="bold red", end="")
+        print("\n" + DIV)
+        print("Lives:", "❤️" * life, "❌" * (MAX_LIFE - life))
+        if life == 0 or score == goal:
+            break
+        while True:
+            print(DIV)
+            letter = input("Enter a letter: ").upper()
+            if letter.isalpha() and len(letter) == 1:
+                if letter in entered_char:
+                    print(DIV, "Letter already entered. Try again.", sep="\n")
+                else:
+                    break
+            else:
+                print(DIV, "Invalid input. Try again", sep="\n")
+        entered_char.append(letter)
+        if letter in alpha_text:
+            score += 1
+        else:
+            life -= 1
+    return {
+        'answer': title.upper(),
+        'life': life,
+        'score': life * 10,
+        'index': movies.index(round_movie)
+    }
+
+
+
+def get_alpha(title):
+    """Removes non-alphabetic characters from a string"
+
+    Args:
+        title (string): the title of the movie to be guessed in the round
+
+    Returns:
+        list: returns a list of all letters in the title
+    """
+    regex = re.compile("A-Z")
+    return regex.findall(title.upper())
 
 
 def clear():
